e22e0fcbfd991282461d70e697b5eb13492973e8
tienleba03 <123140508+tienleba167@users.noreply.github.com>
2023-12-06 23:53:17 -0500

    Project Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom'; 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          CS411 PROJECT
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
